2b8d7d8c12ed7b473cb52e03737a007bbd63ea98
U-tess\cjb60 <cjb60@tess.(none)>
2013-12-15 19:03:42 +1300

    Added code to create folder directories, and other extra tidbits

--- R/chrisplotall.R
+++ R/chrisplotall.R
@@ -16,12 +16,22 @@ init <- function(data) {
   return( list(quantitatives, factors) )
 }
 
+#' Create folder. Will delete folder if it exists before creating new one.
+#' @param str folder name
+createfolder <- function(str) {
+  unlink(str,recursive=TRUE)
+  write(paste("Creating directory called:",str), stdout())
+  dir.create(str)
+}
+
 #' Create scatterplots out of all possible quantitative variables in the data.
 #' @param data the data frame to use
 #' @param no.export if this is set to false, then graphs will be exported to png
 #' @return null
 #' @export
 scatterall <- function(data, no.export=TRUE, ...) {
+  foldername = paste("scatterall_",substitute(loansData),sep="")
+  createfolder(foldername)
   lst <- init(data)
   quantitatives <- lst[[1]]
   factors <- lst[[2]]
@@ -30,7 +40,8 @@ scatterall <- function(data, no.export=TRUE, ...) {
     for( j in i:length(quantitatives)) {
       if( i != j) {
         if(no.export == FALSE) {
-          png(filename=paste("scatter_",paste(i),"_",paste(j),".png",sep=""))
+          png(filename=paste(foldername,"/","scatter_",paste(i),"_",
+                             paste(j),".png",sep=""))
         }
         plot( x=get(quantitatives[i],pos=data),
               y=get(quantitatives[j],pos=data),
@@ -43,6 +54,42 @@ scatterall <- function(data, no.export=TRUE, ...) {
       }
     }
   }
+  write("Done!",stdout())
+}
+
+#' Create scatterplots out of all possible quantitative variables in the data using factors to colour the points.
+#' @param data the data frame to use
+#' @param no.export if this is set to false, then graphs will be exported to png
+#' @return null
+#' @export
+scatterall3d <- function(data, no.export=TRUE, ...) {
+  foldername = paste("scatterall3d_",substitute(loansData),sep="")
+  createfolder(foldername)
+  lst <- init(data)
+  quantitatives <- lst[[1]]
+  factors <- lst[[2]]
+  for( i in 1:length(quantitatives)) {
+    for( j in i:length(quantitatives)) {
+      for( k in 1:length(factors)) {
+        if( i != j) {
+          if(no.export == FALSE) {
+            png(filename=paste(foldername,"/","scatter3d_",paste(i),
+                               "_",paste(j),".png",sep=""))
+          }
+          plot( x=get(quantitatives[i],pos=data),
+                y=get(quantitatives[j],pos=data),
+                xlab=quantitatives[i],
+                ylab=quantitatives[j],
+                col=get(factors[k],pos=data),
+                ...)
+          if(no.export == FALSE) {
+            dev.off()
+          }
+        }
+      }
+    }
+  }
+  write("Done!",stdout())
 }
 
 #' Create histograms out of all possible quantitative variables in the data.
@@ -51,18 +98,21 @@ scatterall <- function(data, no.export=TRUE, ...) {
 #' @return null
 #' @export
 histoall <- function(data, no.export=TRUE) {
+  foldername = paste("hist_",substitute(loansData),sep="")
+  createfolder(foldername)
   lst <- init(data)
   quantitatives <- lst[[1]]
   factors <- lst[[2]]
   for( i in 1:length(quantitatives) ) {
     if( no.export == FALSE ) {
-      png(filename=paste("hist_",paste(i),".png",sep=""))
+      png(filename=paste(foldername,"/","hist_",paste(i),".png",sep=""))
     }
     hist( get(quantitatives[i],pos=data), xlab=quantitatives[i], main=quantitatives[i] )
     if( no.export == FALSE ) {
       dev.off()
     }
   }
+  write("Done!",stdout())
 }
 
 #' Create boxplots of all quantitative variables using all factor variables
@@ -71,13 +121,15 @@ histoall <- function(data, no.export=TRUE) {
 #' @return null
 #' @export
 boxplotall <- function(data, no.export=TRUE, ...) {
+  foldername = paste("boxplot_",substitute(loansData),sep="")
+  createfolder(foldername)
   lst <- init(data)
   quantitatives <- lst[[1]]
   factors <- lst[[2]]
   for( i in 1:length(factors)) {
     for(j in 1:length(quantitatives)) {
       if( no.export == FALSE ) {
-        png(filename=paste("boxplot",paste(i),"_",paste(j),".png",sep=""))
+        png(filename=paste(foldername,"/","boxplot",paste(i),"_",paste(j),".png",sep=""))
       }
       boxplot( get(quantitatives[j],pos=data) ~ get(factors[i],pos=data),
                xlab=factors[i],
@@ -88,4 +140,5 @@ boxplotall <- function(data, no.export=TRUE, ...) {
       }
     }
   }
+  write("Done!",stdout())
 }
